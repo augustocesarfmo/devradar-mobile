9239e1a9e2db51ac58310ceaa8ffebc49862d799
Aleph <aleph@MacBook-Pro-da-Aleph.local>
2020-02-14 09:42:20 -0300

    Socket.io connection

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -15,6 +15,7 @@ import {
 import { MaterialIcons } from "@expo/vector-icons";
 
 import api from "../services/api";
+import { connect, disconnect, subscribeToNewDevs } from "../services/socket";
 
 // import { Container } from './styles';
 
@@ -22,6 +23,13 @@ export default function Main({ navigation }) {
   const [devs, setDevs] = useState([]);
   const [currentRegion, setCurrentRegion] = useState(null);
   const [techs, setTechs] = useState("");
+
+  function setupWebSocket() {
+    disconnect();
+    const { latitude, longitude } = currentRegion;
+
+    connect(latitude, longitude, techs);
+  }
 
   async function loadDevs() {
     const { latitude, longitude } = currentRegion;
@@ -36,6 +44,7 @@ export default function Main({ navigation }) {
     });
 
     setDevs(response.data.devs);
+    setupWebSocket();
     console.log(response.data.devs);
   }
 
@@ -65,6 +74,10 @@ export default function Main({ navigation }) {
     }
     loadInitialPosition();
   }, []);
+
+  useEffect(() => {
+    subscribeToNewDevs(dev => setDevs([...devs, dev]));
+  }, [devs]);
 
   if (!currentRegion) {
     return null;
